6ec308bc13dfdbd07cd6e635a8b7e0252b3c91de
archer <1246506786@qq.com>
2015-09-18 02:23:14 -0400

    scrapy tutorial

---
+++ docs/resource.md
@@ -0,0 +1,2 @@
+[Python scrapy runing bug](http://jackroyal.github.io/2015/04/26/ubuntu-scrapy-stackoverflow/)  
+[Python scrapy tutorial](http://doc.scrapy.org/en/latest/intro/tutorial.html)  
--- test/tutorial/tutorial/spiders/dmoz_spider.py
+++ test/tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from tutorial.items import DmozItem
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
@@ -9,6 +10,9 @@ class DmozSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
