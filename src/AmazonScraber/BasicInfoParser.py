8ee75c8f4e77981d153d88326ad5f2368b09d7d9
archer <1246506786@qq.com>
2015-09-21 23:02:49 -0400

    BasicInfoParser seems worked

--- src/AmazonScraber/BasicInfoParser.py
+++ src/AmazonScraber/BasicInfoParser.py
@@ -8,9 +8,38 @@
 #
 # Produced By BR(BeautifulReading)
 from bs4 import BeautifulSoup
+import json
 
 class BasicInfoParser(object):
+    Soup = None
+    HTML = None
 
     def __init__(self, html):
-        pass
-    
+        self.HTML = html
+        self.Soup = BeautifulSoup(self.HTML)
+
+    def basicInfo(self):
+        infoJson = {}
+        keys = ['出版社:', '外文书名:', '丛书名:', '平装:', '语种：', '开本:', 'ISBN:', '条形码:', '商品尺寸:', '商品重量:', '品牌:', 'ASIN:']
+
+        contentElement = self.Soup.find('td', class_='bucket').find('div', class_='content')
+        if not contentElement:
+            return infoJson
+
+        for item in contentElement.find('ul').find_all('li'):
+            # print item.find('b').get_text().encode('UTF-8').replace('\n', '').replace(' ', '')
+            if item.find('b').get_text().encode('UTF-8') == '外文书名:':
+                # print item.find('b').get_text().encode('UTF-8'), item.find('a').get_text()
+                infoJson[item.find('b').get_text().encode('UTF-8')] = item.find('a').get_text()
+                continue
+
+            if item.find('b').get_text().encode('UTF-8') == '丛书名:':
+                # print item.find('b').get_text().encode('UTF-8'), item.find('a').get_text()
+                infoJson[item.find('b').get_text().encode('UTF-8')] = item.find('a').get_text()
+                continue
+
+            if item.find('b').get_text().encode('UTF-8').replace('\n', '').replace(' ', '') in keys:
+                # print item.find('b').get_text().encode('UTF-8').replace('\n', '').replace(' ', ''), item.find('b').next_sibling.replace('\n', '').replace(' ', '')
+                infoJson[item.find('b').get_text().encode('UTF-8').replace('\n', '').replace(' ', '')] = item.find('b').next_sibling.replace('\n', '').replace(' ', '')
+
+        return json.dumps(infoJson)
