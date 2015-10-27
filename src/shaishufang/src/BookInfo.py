0632b7d5782a632d9e0c17552375c558cad9de92
archer <1246506786@qq.com>
2015-10-27 09:01:46 -0400

    shaishufang, UserBooks and BookInfo class ready

---
+++ src/shaishufang/src/BookInfo.py
@@ -0,0 +1,52 @@
+#!/usr/local/env python
+#
+# Author: Archer Reilly
+# Date: 27/Aug/2015
+# File: BookInfo.py
+# Desc: parse book info page
+#
+# Produced By CSRGXTU
+from Download import Download
+from bs4 import BeautifulSoup
+
+class BookInfo(object):
+    HTML = None
+    UID = None
+    BID = None
+    Cookie = None
+    Soup = None
+
+    def __init__(self, uid, bid, cookie):
+        self.UID = uid
+        self.BID = bid
+        self.Cookie = cookie
+
+        if not self.request():
+            print "ERROR: ", uid, bid, cookie
+        else:
+            self.Soup = BeautifulSoup(self.HTML)
+
+    def getIsbn(self):
+        if not self.Soup:
+            return False
+
+        if self.Soup.find('div', {'id': 'attr'}):
+            if len(self.Soup.find('div', {'id': 'attr'}).find_all('li')) == 0:
+                return False
+            if "ISBN:" in self.Soup.find('div', {'id': 'attr'}).find_all('li')[-1].text:
+                return self.Soup.find('div', {'id': 'attr'}).find_all('li')[-1].text.replace('ISBN:', '')
+            else:
+                return False
+        return False
+
+    def request(self):
+        baseUrl = 'http://shaishufang.com/index.php/site/detail/uid/'
+        postFix = '/status//category/none/friend/false'
+        url = baseUrl + self.UID + '/ubid/' + self.BID + postFix
+
+        d = Download(url, self.Cookie)
+        if d.doRequest():
+            return False
+
+        self.HTML = d.getSOURCE()
+        return True
---
+++ src/shaishufang/src/UserBooks.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 27/Aug/2015
+# File: UserBooks.py
+# Desc: class for UserBooks list page
+#
+# Produced By CSRGXTU
+from Download import Download
+from bs4 import BeautifulSoup
+
+class UserBooks(object):
+    HTML = None
+    Cookie = None
+    UID = None
+    Page = None
+    Soup = None
+
+    def __init__(self, uid, cookie, page):
+        self.UID = uid
+        self.Cookie = cookie
+        self.Page = page
+
+        if not self.request():
+            print "ERROR: ", uid, cookie, page
+        else:
+            self.Soup = BeautifulSoup(self.HTML)
+
+    def getTotalPageNumbers(self):
+        if not self.Soup:
+            return 0
+
+        if self.Soup.find('ul', {'id': 'booksPage'}):
+            if len(self.Soup.find('ul', {'id': 'booksPage'}).find_all('li')) == 0:
+                return 0
+
+            return int(self.Soup.find('ul', {'id': 'booksPage'}).find_all('li')[-2].text)
+
+        return 1
+
+    def getBids(self):
+        bids = []
+        if not self.Soup:
+            return bids
+        
+        if self.Soup.find('ul', {'id': 'booksList'}):
+            if len(self.Soup.find('ul', {'id': 'booksList'}).find_all('li')) == 0:
+                return bids
+            for item in self.Soup.find('ul', {'id': 'booksList'}).find_all('li'):
+                bids.append(item.attrs['id'])
+
+        return bids
+
+    def request(self):
+        baseUrl = "http://shaishufang.com/index.php/site/main/uid/"
+        postFix = "/friend/false/category//status//type//page/"
+        url = baseUrl + self.UID + postFix + str(self.Page)
+
+        d = Download(url, self.Cookie)
+        if d.doRequest():
+            return False
+
+        self.HTML = d.getSOURCE()
+        return True
