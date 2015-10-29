cad7a615f1f6dec74dfcaf1ea3e19e19422b5a07
archer <1246506786@qq.com>
2015-10-29 02:19:48 -0400

    add multiprocessing support

--- src/shaishufang/src/main.py
+++ src/shaishufang/src/main.py
@@ -6,17 +6,28 @@
 # Desc: main File
 #
 # Produced By CSRGXTU
+import multiprocessing
+
 from UserIsbns import UserIsbns
 from Utility import appendlst2file
 
-def run():
-    uid = '90661'
+def worker(start, offset):
     cookie = 'shaishufang=Mjc5MTYwfGZmY2VmYzIyYmMxZjhlZThjNzgzYjFlOGIxOWUwODg2'
 
-    for i in range(1, 279653):
+    for i in range(start, start + offset):
         ui = UserIsbns(str(i), cookie)
         isbns = ui.run()
         appendlst2file(isbns, 'isbns.txt')
+
+def run():
+    jobs = []
+
+    index = 1
+    for i in range(50):
+        p = multiprocessing.Process(target=worker, args=(index, 5593))
+        jobs.append(p)
+        p.start()
+        index = index + 5593
 
 if __name__ == '__main__':
     run()
