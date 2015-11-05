e8b35b0830ccd5518a73ba9400e73c18e5e938b8
archer <1246506786@qq.com>
2015-11-05 00:39:10 -0500

    tem commit

---
+++ src/shaishufang/src/mainUid.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 28/Oct/2015
+# File: main.py
+# Desc: main File
+#
+# Produced By CSRGXTU
+import multiprocessing
+
+from UserIsbns import UserIsbns
+from Utility import saveLstToFile
+
+def worker(start, offset):
+    cookie = 'shaishufang=Mjc5MTYwfGZmY2VmYzIyYmMxZjhlZThjNzgzYjFlOGIxOWUwODg2'
+
+    for i in range(start, start + offset):
+        ui = UserIsbns(str(i), cookie)
+        isbns = ui.run()
+        saveLstToFile('isbns.txt', isbns)
+
+def run():
+    jobs = []
+
+    index = 1
+    for i in range(50):
+        p = multiprocessing.Process(target=worker, args=(index, 5593))
+        jobs.append(p)
+        p.start()
+        index = index + 5593
+
+if __name__ == '__main__':
+    #run()
+    worker(10393, 1)
