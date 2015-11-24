ac8b729ece19d9663fa1eac258bf6f0b17534422
archer <1246506786@qq.com>
2015-11-24 11:13:40 +0800

    script insert 6w 2 Sauron master

---
+++ tool/KaiJuanSauron.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Author: Archer Reilly
+# File: KaiJuanSauron.py
+# Date: 24/Nov/2015
+# Desc: get isbns from /data/kaijuan.csv and build up
+#     douban book url and insert into Sauron master db
+#
+# Produced By BR
+import unirest
+import json
+
+BaseUrl = 'http://book.douban.com/isbn/'
+
+with open('../data/kaijuan.csv', 'r') as FH:
+    FH.readline() # remove first line, the header
+    counter = 0
+    URLS = []
+
+    for line in FH:
+        if counter == 10 or line == '':
+            data = {'urls': []}
+            for url in URLS:
+                data['urls'].append({'url': url, 'spider': '6w'})
+
+            if putUnvisitedUrls(data):
+                print 'Inserted: ', data
+            else:
+                print 'Fail: ', data
+
+            counter = 0
+            URLS = []
+
+        URLS.append(BaseUrl + line.strip('\n').split(',')[0])
+        counter = counter + 1
+
+# putUnvisitedUrls will put data dict to master
+# data: {'urls': [{'url': 'http://w.g.com', 'spider': 'Shaishufan'}]}
+def putUnvisitedUrls(data):
+    url = 'http://192.168.100.3:5000/unvisitedurls'
+    unirest.timeout(180) # time out 180 same with scrapy download middlewares
+    res = unirest.put(url, headers=Headers, params=json.dumps(data))
+
+    if res.body['code'] != 200:
+        return False
+
+    return True
