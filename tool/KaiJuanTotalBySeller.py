9eb96a36b8d4545f4bdabb4654835c08754364c6
archer <1246506786@qq.com>
2015-12-22 08:27:31 +0800

    KaiJuanTotalBySeller.py temp

--- tool/KaiJuanTotalBySeller.py
+++ tool/KaiJuanTotalBySeller.py
@@ -6,6 +6,27 @@
 # Desc: 对开卷的所有书籍，统计各个图书商的总销售价钱
 #
 # Produced By BR
+import unirest
+import json
+
+Headers = {
+    # 'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+# get data from Sauron's datas according to the spider name
+#
+# spider
+# return dict
+def getDatas(spider, start, offset):
+    url = 'http://192.168.100.3:5000/data?start=' + start + '&offset=' + offset + '&spider=' + spider
+    unirest.timeout(180)
+    res = unirest.get(url, headers=Headers)
+
+    if res.body['code'] != 200:
+        return False
+
+    return res.body['data']
 
 # Get Data from Sauron's Data
 
@@ -13,3 +34,6 @@
 
 # When matrix is ready, count total for each seller,
 # and put it into matrix, and put it into file
+
+if __name__ == '__main__':
+    getDatas('6w', 0, 1)
