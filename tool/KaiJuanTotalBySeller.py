dfe522d5606f8b528f01584ccdf688d29e2e6c55
archer <1246506786@qq.com>
2015-12-22 08:16:12 +0800

    KaiJuanTotalBySeller.py

---
+++ tool/KaiJuanTotalBySeller.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Author: Archer Reilly
+# File: KaiJuanTotalBySeller.py
+# Date: 22/Dec/2015
+# Desc: 对开卷的所有书籍，统计各个图书商的总销售价钱
+#
+# Produced By BR
+
+# Get Data from Sauron's Data
+
+# Foreach record, process it and put into matrix
+
+# When matrix is ready, count total for each seller,
+# and put it into matrix, and put it into file
