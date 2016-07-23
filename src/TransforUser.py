153e233346674fcdfe5ad2539bb22144ada59b8a
archer <1246506786@qq.com>
2016-07-23 17:11:45 +0800

    can vertex user

--- src/TransforUser.py
+++ src/TransforUser.py
@@ -8,14 +8,31 @@
 #
 # Produced By BR
 from pymongo import MongoClient
+import pyorient
 
+# connect to mongodb
 client = MongoClient('mongodb://linyy:rioreader@192.168.200.22:27017/bookshelf')
 db = client['bookshelf']
 uc = db['user']
 
+# connect to orientdb
+orientClient = pyorient.OrientDB("192.168.100.2", 2424)
+orientSession = orientClient.connect( "root", "archer" )
+orientClient.db_open('bookshelf', "root", "archer" )
+
+
 users = uc.find({'user_name': {'$exists': 1}})
 for user in users:
+    command = ''
     if 'avatar' in user:
         print user['user_id'], user['user_name'], user['avatar']
+        command = 'create vertex User set user_id="' + user['user_id'] + '", user_name=\'' + user['user_name'].replace('\'', '').replace('\n', '').replace('\\', '') + '\', avatar="' + user['avatar'] + '"'
     else:
         print user['user_id'], user['user_name'], 'no avatar'
+        command = 'create vertex User set user_id="' + user['user_id'] + '", user_name=\'' + user['user_name'].replace('\'', '').replace('\n', '').replace('\\', '') + '\''
+
+    orientClient.command(command)
+
+
+# 释放orient资源
+orientClient.db_close()
