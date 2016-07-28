bb848038aabcf692887dcd56cc129cbbeccdd486
archer <1246506786@qq.com>
2016-07-28 15:41:13 +0800

    fuckit

--- src/RetrieveDataFromBookful2CSV.py
+++ src/RetrieveDataFromBookful2CSV.py
@@ -15,27 +15,36 @@ import csv
 client = MongoClient('mongodb://127.0.0.1:27017/bookshelf')
 db = client['bookshelf']
 bc = db['bookful']
+libc = db['library']
 
 CSVFile = open('data.csv', 'wt')
 writer = csv.writer(CSVFile)
-writer.writerow( ('bid', 'rating', 'price', 'pubdate', 'publisher') )
-books = bc.find({'bid': {'$exists': 1}, 'rating': {'$exists': 1}, 'price': {'$exists': 1}, 'pubdate': {'$exists': 1}, 'publisher': {'$exists': 1, '$ne': None}})
-for book in books:
+writer.writerow( ('bid', 'rating', 'price', 'pages', 'pubdate') )
+# books = bc.find({'bid': {'$exists': 1}, 'rating': {'$exists': 1}, 'price': {'$exists': 1}, 'pubdate': {'$exists': 1}, 'publisher': {'$exists': 1, '$ne': None}})
+# libs = libc.find({'bid': {'$exists': 1}, 'price': {'$exists': 1}, 'pages': {'$exists': 1}, 'pubdate': {'$exists': 1}})
+libs = libc.find({'bid': {'$exists': 1}})
+# for book in books:
+for book in libs:
     print book['_id']
     bid = book['bid']
-    if 'rating' in book and book['rating'] and 'average' in book['rating']:
-        rating = book['rating']['average'] + '0'
+    if 'rating' in book:
+        try:
+            rating = re.findall('[\d\.]+', book['rating'])[0]
+        except:
+            rating = 0
+        # rating = book['rating'] + '0'
     else:
         rating = 0
 
-    if book['price'] == "" or not book['price']:
-        price = 0
-    else:
-        tmp = re.findall('[\d\.]+', book['price'])
+    if 'price' in book and book['price']:
+        tmp = re.findall('[\d]+\.{1}', book['price'])
         if len(tmp) == 0:
             price = 0
         else:
             price = tmp[0].replace('..', '.') + '0'
+    else:
+        price = 0
+
     try:
         if book['pubdate'] and book['pubdate'] != "":
             pubdate = re.findall('[\d\-]+', book['pubdate'])[0]
@@ -44,13 +53,19 @@ for book in books:
     except:
         pubdate = '0000-00-00'
 
-    publisher = book['publisher']
+    bookful = bc.find_one({'bid': bid, 'pages': {'$exists': 1}})
+    if bookful:
+        try:
+            if bookful['pages'] != "":
+                pages = re.findall('[\d\.]+', bookful['pages'])[0]
+            else:
+                pages = 0
+        except:
+            pages = 0
+    else:
+        pages = 0
 
-    print rating, price
-    writer.writerow( (bid, float(rating), float(price), pubdate, publisher.encode('utf-8')) )
-    # record = bid + ',' + str(rating) + ',' + str(price) + ',' +? str(pubdate) + ',' + publisher
-    # print type(record)
-    # CSVFile.write(record.encode('utf-8'))
-    # CSVFile.write('\n')
+    print bid, float(rating), float(price), float(pages), pubdate
+    writer.writerow( (bid, float(rating), float(price), float(pages), pubdate, ) )
 
 CSVFile.close()
